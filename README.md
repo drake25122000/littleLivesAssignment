920cc1eced5a2b1e164c21ae07716403bdc6bd2c
drake25122000 <drake251@comp.nus.edu.sg>
2021-02-22 12:06:34 +0800

    V1

---
+++ README.md
@@ -0,0 +1,34 @@
+# Project Title
+
+Forming a Magic Square
+
+## Description
+
+How to turn any matrix into matrix that satisfies Magic Square properties
+
+## Getting Started
+
+### Dependencies
+
+* Describe any prerequisites, libraries, OS version, etc., needed before installing program.
+* ex. Windows 10
+
+### Executing program
+
+* How to run the program
+* Step-by-step bullets
+```
+code blocks for commands
+```
+
+## Authors
+
+Contributors names and contact info
+
+Christian Drake Martin
+[@drake251200](http://https//www.linkedin.com/in/christian-drake-martin/)
+
+## Version History
+
+* 0.1
+    * Initial Release
---
+++ formingMagicSquare.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class Main
+{
+    
+    /**
+     * Forming A Magic Square
+     * Since all the numbers in the 3x3 array must be different
+     * There are only 8 array combinations that satisfy the Magic Square property
+     */
+    
+    //These are the 8 array combinations
+    public static int[][] magicSquare = {
+        {4,9,2,3,5,7,8,1,6},
+        {4,3,8,9,5,1,2,7,6},
+        {2,9,4,7,5,3,6,1,8},
+        {2,7,6,9,5,1,4,3,8},
+        {8,1,6,3,5,7,4,9,2},
+        {8,3,4,1,5,9,6,7,2},
+        {6,7,2,1,5,9,8,3,4},
+        {6,1,8,7,5,3,2,9,4},
+    };
+    
+    /**
+     * To calculate the cost to form magicSquare array
+     * @param flattenArray 
+     * */
+    public static int formMagicSquareCost(int[] flattenArray) {
+        int cost = Integer.MAX_VALUE;       //Keep track of the minimum cost
+        
+        //Loop through every combinations
+        for (int i = 0; i < 8 ; i++) {
+            int tempCost = 0;
+            for (int j = 0 ; j < 9 ; j++) {
+                int different = Math.abs(flattenArray[j] - magicSquare[i][j]);
+                tempCost = tempCost + different;
+            }
+            
+            cost = tempCost < cost ? tempCost : cost;
+        }
+        
+        return cost;
+    }
+    
+    /**
+     * To convert 2D array to 1D array
+     * @param arr
+     * */
+    public static int[] flattenArray(int[][] arr) {
+        int[] out = new int[9];
+        int pointer = 0;
+        
+        for (int i = 0 ; i < 3 ; i++) {
+            for (int j = 0 ; j < 3 ; j++) {
+                out[pointer] = arr[i][j];
+                pointer++;
+            }
+        }
+        
+        return out;
+    }
+     
+	public static void main(String[] args) {
+	    
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("Enter array elements : ");    
+        
+        int inputArr[][] = new int[3][3];
+        
+          
+        for (int i = 0 ; i < 3 ; i++) {            
+            for (int j = 0 ; j < 3 ; j++) {
+                inputArr[i][j] = scan.nextInt();
+            }
+        }
+        System.out.print("Minimum cost to form Magic Square: " + (formMagicSquareCost(flattenArray(inputArr))));
+        
+	    
+	}
+}
